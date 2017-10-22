920db0a7506e5fc10291793379ff8bfc5e26dc1d
Franco <valterpassion@hotmail.com>
2017-10-22 21:14:46 -0200

    updates

--- src/main/java/org/me/ws/PatientResource.java
+++ src/main/java/org/me/ws/PatientResource.java
@@ -1,14 +1,18 @@
 package org.me.ws;
 
+import java.io.File;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import javax.activation.MimetypesFileTypeMap;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import org.me.pi4.model.Patient;
 import org.me.pi4.modeldao.PatientDAO;
 
@@ -37,6 +41,16 @@ public class PatientResource{
     return p;
     }
     
-    
-    
+     @GET
+     @Path("/images/{image}")
+     public Response getImage(@PathParam("image") String image) {
+     File f = new File(image);
+ 
+     if (!f.exists()) {
+     throw new WebApplicationException(404);
+     }
+  
+     String mt = new MimetypesFileTypeMap().getContentType(f);
+     return Response.ok(f, mt).build();
+     }
 }
